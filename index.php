93eec11ce93cdb564cb29211bcac114188bfb8ab
Parth Agarwal <parth.agarwal131@gmail.com>
2020-12-22 21:48:55 +0530

    rollout changes

--- index.php
+++ index.php
@@ -1,55 +1,23 @@
-<html>
-<head>
-	<title>Hello world!</title>
-	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
-	<style>
-	body {
-		background-color: #2989A4;
-		text-align: center;
-		padding: 50px;
-		font-family: "Open Sans","Helvetica Neue",Helvetica,Arial,sans-serif;
-    color: white;
-	}
+<?php
+header("Content-Type: application/json");
 
-	#logo {
-		margin-bottom: 40px;
-	}
-	</style>
-</head>
-<body>
-	<img id="logo" src="logo.png" />
-	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
-	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
-	<?php
-	$links = [];
-	foreach($_ENV as $key => $value) {
-		if(preg_match("/^(.*)_PORT_([0-9]*)_(TCP|UDP)$/", $key, $matches)) {
-			$links[] = [
-				"name" => $matches[1],
-				"port" => $matches[2],
-				"proto" => $matches[3],
-				"value" => $value
-			];
-		}
-	}
-	if($links) {
-	?>
-		<h3>Links found</h3>
-		<?php
-		foreach($links as $link) {
-			?>
-			<b><?php echo $link["name"]; ?></b> listening in <?php echo $link["port"]+"/"+$link["proto"]; ?> available at <?php echo $link["value"]; ?><br />
-			<?php
-		}
-		?>
-	<?php
-	}
+$data = {
+	"color": "red",
+	"status": "ok"
+  };
 
-	if($_ENV["DOCKERCLOUD_AUTH"]) {
-		?>
-		<h3>I have Docker Cloud API powers!</h3>
-		<?php
-	}
-	?>
-</body>
-</html>
+
+$json = json_encode($data);
+if ($json === false) {
+    // Avoid echo of empty string (which is invalid JSON), and
+    // JSONify the error message instead:
+    $json = json_encode(["jsonError" => json_last_error_msg()]);
+    if ($json === false) {
+        // This should not happen, but we go all the way now:
+        $json = '{"jsonError":"unknown"}';
+    }
+    // Set HTTP response status code to: 500 - Internal Server Error
+    http_response_code(500);
+}
+echo $json;
+?>
